09e396d89b6172a78299070ba07c816e2067aa7c
Henry Vega <hp.vega21@gmail.com>
2025-11-26 21:43:52 -0500

    Remove unused data source for availability zones in vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -118,8 +118,3 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
-
-# Data source para availability zones
-data "aws_availability_zones" "available" {
-  state = "available"
-}
